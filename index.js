71208d0f6bbcb1188e86bbd59bf551a9644388fe
Jason Nichols <jasonnic@google.com>
2022-08-11 08:06:10 -0400

    update to index.js to show promotion

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
+  console.log(`helloworld-uat: listening on port ${port}`);
 });
